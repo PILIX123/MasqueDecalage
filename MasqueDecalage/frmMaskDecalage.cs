19b953ae098267fc02323fc0b7ab9ab7f1820c92
Azoki <victorsaulnier5@gmail.com>
2021-09-16 17:23:00 -0400

    Revert "Écriture bouton date"
    This reverts commit 8bac27c7da2cd6c977443db2a9ef2bb7c9990873.

--- MasqueDecalage/frmMaskDecalage.cs
+++ MasqueDecalage/frmMaskDecalage.cs
@@ -28,6 +28,7 @@ namespace MasqueDecalage
             uint maskAn = 0xFE000000;
             uint maskMois = 0x01E00000;
             uint maskJour = 0x001F0000;
+            string date = "";
             string[] tMois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
             uint Jour, Mois, An;
 
@@ -37,11 +38,9 @@ namespace MasqueDecalage
             Jour = (uint)((value & maskJour) / Math.Pow(2, 16));
 
 
-            txtDate.Text = Jour.ToString() + "/" + tMois[Mois -1] + "/" + An.ToString();
+
 
             indiceDate++;
-            if (indiceDate > 9)
-                indiceDate = 0;
         }
         private void btnHeurre_Click(object sender, EventArgs e)
         {
