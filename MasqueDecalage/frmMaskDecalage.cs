4bf40c763e2147a3d7bd5424b9e587c9e9e79fe8
Pierre-Luc Robitaille <2012805@cegeplevis.ca>
2021-09-26 16:57:49 -0400

    avoir des 0 avant les chiffres a unit. simple

--- MasqueDecalage/frmMaskDecalage.cs
+++ MasqueDecalage/frmMaskDecalage.cs
@@ -33,14 +33,17 @@ namespace MasqueDecalage
             uint maskJour = 0x001F0000;
             string[] tMois = { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
             uint Jour, Mois, An;
-
+            string jour;
             uint value = tDateHeure[indiceDate];
             An = (uint)((value & maskAn) / Math.Pow(2, 25) + 1900);
             Mois = (uint)((value & maskMois) / Math.Pow(2, 21));
             Jour = (uint)((value & maskJour) / Math.Pow(2, 16));
+            if (Jour < 10)
+                jour = "0" + Jour.ToString();
+            else
+                jour = Jour.ToString();
 
-
-            txtDate.Text = Jour.ToString() + "/" + tMois[Mois - 1] + "/" + An.ToString();
+            txtDate.Text = jour + "/" + tMois[Mois - 1] + "/" + An.ToString();
 
             indiceDate++;
             if (indiceDate > 9)
@@ -50,10 +53,29 @@ namespace MasqueDecalage
         {
             uint value = tDateHeure[indiceHeure];
             uint Heures, Minutes, Secondes;
+            string heure,minute,seconde;
             Heures = (uint)(((((value << 16) >> 16) >> 11) << 11) / Math.Pow(2, 11));
             Minutes = (uint)(((((value << 21) >> 21) >> 5) << 5) / Math.Pow(2, 5));
             Secondes = (uint)((((value << 27) >> 27) / Math.Pow(2, 0)) * 2);
-            textHeure.Text = Heures.ToString() + ":" + Minutes.ToString() + ":" + Secondes.ToString();
+            if (Heures == 0)
+                heure = "00";
+            else if (Heures < 10)
+                heure = "0" + Heures.ToString();
+            else
+                heure = Heures.ToString();
+            if (Minutes == 0)
+                minute = "00";
+            else if (Minutes < 10)
+                minute = "0" + Minutes.ToString();
+            else
+                minute = Minutes.ToString();
+            if (Secondes == 0)
+                seconde = "00";
+            else if (Secondes < 10)
+                seconde = "0" + Secondes.ToString();
+            else
+                seconde = Secondes.ToString();
+            textHeure.Text = heure + ":" + minute + ":" + seconde;
             indiceHeure++;
             if (indiceHeure > 9)
                 indiceHeure = 0;
